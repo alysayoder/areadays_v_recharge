57523a35f21f15dba9a63ce99ae12fdf296d62f5
alysayoder <31457869+alysayoder@users.noreply.github.com>
2019-02-06 11:35:04 -0800

    Script now functioning properly.
    Calculated area*days for each recharge event.

--- inund_events2019/aday_evsub_fxn.R
+++ inund_events2019/aday_evsub_fxn.R
@@ -1,5 +1,5 @@
 #function needs to query the event_pre dataframe to get the beginning and end of each event 
-#subset discharge data to the length of each flooding event
+#subset inundation area data to the length of each flooding event
 
 evsub <- function(d_start, d_end) {
   library(lubridate)
@@ -7,7 +7,8 @@ evsub <- function(d_start, d_end) {
   library(here)
   #get data
   inundata <- read.csv(here::here("data", "Whipple_Hydrospatial_PostRestoration_wy2013-2017.csv"), stringsAsFactors = FALSE)
-  inundata$dt <- as.POSIXct(inundata$dt, format = "%m/%d/%Y %H:%M")
+  inundata <- inundata[,-c(2:3,5:6)]
+  inundata$dt <- as.POSIXct(inundata$dt, format = "%m/%d/%Y")
   inundata %>% 
     dplyr::select(dt, area_km2) %>%
     dplyr::filter(dt >= as.POSIXct(d_start) & dt <= as.POSIXct(d_end))
